8513b6063dfe7bcc5772d58c1ec0e145ef1bc4ac
Benjamin <ben.d.maryland@gmail.com>
2021-09-28 08:22:47 -0400

    commit tree d00ad744f1aa9468c36f24d96cddc198042bc68b

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 function addingEventListener() {
 const input = document.getElementById('input');
 input.addEventListener('click', function() {
-  alert('I was clicked!');
+  alert('I was a clicked!');
+  console.log("hi")
 });     
 }
+
+const button =  document.createElement("BUTTON");
+console.log(button)
+button.innerHTML = "CLICK ME"; 
+document.body.appendChild(button)
+button.addEventListener(`click`, elem=> console.log(elem.target))
